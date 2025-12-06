ef9500768fba876ca2f6ef2da135f124f7eb9232
Logger <i-am-logger@users.noreply.github.com>
2025-12-05 22:46:28 -0700

    fix: Resolve infinite recursion and undefined cfg in development.nix
    - Fixed circular dependency with Docker configuration
    - Removed undefined cfg references for binfmt and k3s
    - k3s config now handled by my.infra.k3s module directly
    - Binfmt and AppImage support always enabled for dev users

--- my/users/dev/development.nix
+++ my/users/dev/development.nix
@@ -9,8 +9,8 @@ let
   # Get list of all user names
   userNames = attrNames config.my.users;
 
-  # Auto-enable Docker infrastructure when any user has dev = true
-  dockerCfg = config.my.infra.docker;
+  # Docker should be enabled when any user has dev = true OR explicitly enabled
+  dockerEnabled = anyUserDev || (config.my.infra.docker.enable or false);
 in
 {
   config = mkIf anyUserDev (mkMerge [
@@ -22,12 +22,13 @@ in
           extraGroups = [ "disk" "dialout" ];
         })
         (filterAttrs (name: userCfg: userCfg.fullName or null != null) config.my.users);
+
+      # Auto-enable Docker infrastructure when any user has dev = true
+      my.infra.docker.enable = mkDefault true;
     }
 
-    # Docker support (auto-enabled by user dev feature)
-    (mkIf (dockerCfg.enable or anyUserDev) {
-      # Auto-enable Docker infrastructure
-      my.infra.docker.enable = mkDefault true;
+    # Docker support (enabled when dockerEnabled is true)
+    (mkIf dockerEnabled {
       environment.systemPackages = with pkgs; [
         docker-compose
         minikube
@@ -55,15 +56,13 @@ in
       };
     })
 
-    # Binfmt emulation support
-    (mkIf cfg.binfmt.enable {
+    # Binfmt emulation support - always enable for dev users
+    {
       boot.binfmt = {
         emulatedSystems = [ "aarch64-linux" ];
       };
-    })
 
-    # AppImage support (now under binfmt)
-    (mkIf cfg.binfmt.appimage {
+      # AppImage support
       boot.binfmt.registrations.appimage = {
         wrapInterpreterInShell = false;
         interpreter = "${pkgs.appimage-run}/bin/appimage-run";
@@ -72,59 +71,6 @@ in
         mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
         magicOrExtension = ''\x7fELF....AI\x02'';
       };
-    })
-
-    # k3s Kubernetes cluster for local development
-    (mkIf cfg.k3s.enable {
-      # Enable k3s
-      services.k3s = {
-        enable = true;
-        role = cfg.k3s.role;
-        extraFlags = toString (
-          optionals cfg.k3s.disableTraefik [ "--disable=traefik" ]
-        );
-      };
-
-      # Install essential k8s tools
-      environment.systemPackages = with pkgs; [
-        kubectl
-        kubernetes-helm
-        k3s
-      ];
-
-      # Set KUBECONFIG environment variable system-wide
-      environment.variables = {
-        KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
-      };
-
-      # Open API port and trust k3s network interfaces
-      networking.firewall = {
-        allowedTCPPorts = [ cfg.k3s.apiPort ];
-        trustedInterfaces = [ "cni0" "flannel.1" ];
-      };
-
-      # Make kubeconfig readable by users
-      systemd.services.k3s-kubeconfig-permissions = mkIf cfg.k3s.kubeconfigReadable {
-        description = "Set k3s kubeconfig permissions";
-        after = [ "k3s.service" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-        };
-        script = ''
-          while [ ! -f /etc/rancher/k3s/k3s.yaml ]; do
-            sleep 1
-          done
-          chmod 644 /etc/rancher/k3s/k3s.yaml
-        '';
-      };
-
-      # Prevent NetworkManager from managing k3s interfaces
-      environment.etc."NetworkManager/conf.d/k3s-unmanaged.conf".text = ''
-        [keyfile]
-        unmanaged-devices=interface-name:cni*;interface-name:flannel*;interface-name:veth*
-      '';
-    })
+    }
   ]);
 }
