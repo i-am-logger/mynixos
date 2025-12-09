734c35ed14dd66e4a66a78af4498f4c975cdf7ab
Ido Samuelson <i-am-logger@users.noreply.github.com>
2025-12-08 23:03:49 -0700

    Fix fastfetch alignment to 10 characters and add terminal font
    - Align all keys to 10 character width (longest is 'Font (GTK)')
    - OS: 8 spaces, Host: 6, Uptime/Kernel: 4, Shell: 5, Packages: 2
    - CPU/GPU: 7 spaces, Memory: 4, Disk/Swap: 6, Display/Battery: 3
    - WM: 8 spaces, Theme: 5, Font (Qt): 1, Font (GTK): 0, Terminal: 2
    - Add terminalfont module to show terminal font (if supported)
    - Perfect right-aligned column for all sections

--- my/users/apps/sysinfo/fastfetch/default.nix
+++ my/users/apps/sysinfo/fastfetch/default.nix
@@ -44,23 +44,23 @@ in
                 },
                 {
                   "type": "os",
-                  "key": "      \u001b[1mOS\u001b[0m"
+                  "key": "        \u001b[1mOS\u001b[0m"
                 },
                 {
                   "type": "host",
-                  "key": "    \u001b[1mHost\u001b[0m"
+                  "key": "      \u001b[1mHost\u001b[0m"
                 },
                 {
                   "type": "uptime",
-                  "key": "  \u001b[1mUptime\u001b[0m"
+                  "key": "    \u001b[1mUptime\u001b[0m"
                 },
                 {
                   "type": "kernel",
-                  "key": "  \u001b[1mKernel\u001b[0m"
+                  "key": "    \u001b[1mKernel\u001b[0m"
                 },
                 {
                   "type": "shell",
-                  "key": "   \u001b[1mShell\u001b[0m"
+                  "key": "     \u001b[1mShell\u001b[0m"
                 },
                 {
                   "type": "packages",
@@ -73,32 +73,32 @@ in
                 },
                 {
                   "type": "cpu",
-                  "key": "     \u001b[1mCPU\u001b[0m"
+                  "key": "       \u001b[1mCPU\u001b[0m"
                 },
                 {
                   "type": "gpu",
-                  "key": "     \u001b[1mGPU\u001b[0m"
+                  "key": "       \u001b[1mGPU\u001b[0m"
                 },
                 {
                   "type": "memory",
-                  "key": "  \u001b[1mMemory\u001b[0m"
+                  "key": "    \u001b[1mMemory\u001b[0m"
                 },
                 {
                   "type": "disk",
-                  "key": "    \u001b[1mDisk\u001b[0m",
+                  "key": "      \u001b[1mDisk\u001b[0m",
                   "hideFolders": []
                 },
                 {
                   "type": "swap",
-                  "key": "    \u001b[1mSwap\u001b[0m"
+                  "key": "      \u001b[1mSwap\u001b[0m"
                 },
                 {
                   "type": "display",
-                  "key": " \u001b[1mDisplay\u001b[0m"
+                  "key": "   \u001b[1mDisplay\u001b[0m"
                 },
                 {
                   "type": "battery",
-                  "key": " \u001b[1mBattery\u001b[0m"
+                  "key": "   \u001b[1mBattery\u001b[0m"
                 },
                 "break",
                 {
@@ -107,11 +107,11 @@ in
                 },
                 {
                   "type": "wm",
-                  "key": "      \u001b[1mWM\u001b[0m"
+                  "key": "        \u001b[1mWM\u001b[0m"
                 },
                 {
                   "type": "theme",
-                  "key": "   \u001b[1mTheme\u001b[0m"
+                  "key": "     \u001b[1mTheme\u001b[0m"
                 },
                 {
                   "type": "font",
@@ -126,6 +126,10 @@ in
                 {
                   "type": "terminal",
                   "key": "  \u001b[1mTerminal\u001b[0m"
+                },
+                {
+                  "type": "terminalfont",
+                  "key": " \u001b[1mTerm Font\u001b[0m"
                 }
               ]
             }
